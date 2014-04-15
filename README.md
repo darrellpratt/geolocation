22d4647c2a861552f941f1d093e6b01200d3813d
Darrell Pratt <darrell.pratt@gmail.com>
2014-04-14 22:55:25 -0500

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# geolocation
+
+GIS functions
+
+[![build status](https://secure.travis-ci.org/darrellpratt/geolocation.png)](http://travis-ci.org/darrellpratt/geolocation)
+
+## Installation
+
+```
+npm install geolocation --save
+```
+
+## Usage
+
+## Credits
+[Darrell Pratt](https://github.com/darrellpratt/)
---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict'
+
+function GeoLocation (latitude, longitude) {
+
+  // functions
+  this.fromRadians = function(latitude, longitude) {
+
+    var radLat = latitude;
+    var radLon = longitude;
+    var degLat = latitude * 180/Math.PI; // degrees = radians * (180/pi)
+    var degLon = longitude * 180/Math.PI; // degrees = radians * (180/pi)
+
+    return [degLon,degLat];
+  };
+
+  /** Converts numeric degrees to radians */
+  if (typeof Number.prototype.toRad == 'undefined') {
+    Number.prototype.toRad = function() {
+      return this * Math.PI / 180;
+    }
+  };
+
+  /** Converts radians to numeric (signed) degrees */
+  if (typeof Number.prototype.toDeg == 'undefined') {
+    Number.prototype.toDeg = function() {
+      return this * 180 / Math.PI;
+    }
+  };
+
+  this.version = '0.1';
+  this.degLon = longitude;
+  this.degLat = latitude;
+  this.earthRadius = 6371.01;
+  this.radLon = this.degLon.toRad();
+  this.radLat = this.degLat.toRad();
+
+  this.MIN_LAT = (-90).toRad();  // -PI/2
+  this.MAX_LAT = (90).toRad();   //  PI/2
+  this.MIN_LON = (-180).toRad(); // -PI
+  this.MAX_LON = (180).toRad();
+
+};
+
+/*
+ * Return a bounding box in form [minX,minY,maxX,maxY]
+ *   x = longitude,  y = lattitude & is lowerleft x,y, upper right x,y
+ */
+GeoLocation.prototype.boundingBox = function(radKm) {
+  console.log('geolocation class');
+  console.log(this.degLon);
+  console.log(radKm);
+  console.log(this.degLat);
+
+  var radDist = radKm / this.earthRadius;
+  var minLat = this.radLat - radDist;
+  var maxLat = this.radLat + radDist;
+
+  console.log(radDist);
+
+  if (minLat > this.MIN_LAT && maxLat < this.MAX_LAT) {
+    var deltaLon = Math.asin(Math.sin(radDist) /
+      Math.cos(this.radLat));
+    var minLon = this.radLon - deltaLon;
+    if (minLon < this.MIN_LON) minLon += 2 * Math.PI;
+    var maxLon = this.radLon + deltaLon;
+    if (maxLon > this.MAX_LON) maxLon -= 2 * Math.PI;
+  } else {
+    // a pole is within the distance
+    minLat = Math.max(minLat, this.MIN_LAT);
+    maxLat = Math.min(maxLat, this.MAX_LAT);
+    var minLon = this.MIN_LON;
+    var maxLon = this.MAX_LON;
+  };
+  var lowerLeft = this.fromRadians(minLat,minLon);
+  var upperRight = this.fromRadians(maxLat,maxLon);
+
+  return [lowerLeft[0],lowerLeft[1],upperRight[0],upperRight[1]].toString();
+
+};
+
+
+
+module.exports = GeoLocation;
